9cfc3cccddd3582e1b53a6042e3d39ae14f8d847
Elaine Anderson <eparie@bu.edu>
2018-07-16 19:18:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,16 +13,5 @@ get '/pirates' do
   erb :pirates
 end
 
-    post '/new' do
-@pirate = Pirate.new(params[:pirate])
-
-params[:pirate][:ships].each do |details|
-  Ship.new(details)
-end
-
-@ships = Ship.all
-
-erb :show
-    end
-  end
+    
 end
