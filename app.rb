3fb44d6b848753323cf378bd4d14b7cffa33743f
Elaine Anderson <eparie@bu.edu>
2018-07-16 19:03:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require './environment'
-require_relative 'models/pirate'
-require_relative 'models/ship'
+require_relative 'app/models/pirate'
+require_relative 'app/models/ship'
 
 module FormsLab
   class App < Sinatra::Base
