55c19d0ed6598412fc3301b041bce21cf2a13bae
Elaine Anderson <eparie@bu.edu>
2018-07-16 18:40:35 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  attr_reader :name, :topic
+
+COURSES = []
+
+def initialize(args)
+  @name = args[:name]
+  @topic = args[:topic]
+  COURSES << self
+end
+
+def self.all
+  COURSES
+end
+
 end
